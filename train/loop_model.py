a71f31cc503bc0c1132f24580185adc7c8ac4042
joann8512 <joann8512@gmail.com>
2020-10-06 11:21:44 +0800

    organized files

---
+++ train/loop_model.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 15:24:34 2020
+
+@author: Joann
+"""
+#%%
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from loop_modules import HarmonicSTFT
+
+#%%
+
+class Model(nn.Module):
+    def __init__(self,
+                conv_channels=128,
+                sample_rate=16000,
+                n_fft=513,#513
+                n_harmonic=6,
+                semitone_scale=2,
+                learn_bw=None):  # bw = bandwidth
+        super(Model, self).__init__()
+        self.hstft = HarmonicSTFT(sample_rate=sample_rate,
+                                    n_fft=n_fft,
+                                    n_harmonic=n_harmonic,
+                                    semitone_scale=semitone_scale,
+                                    learn_bw=learn_bw)
+        self.hstft_bn = nn.BatchNorm2d(n_harmonic)
+
+        # 2D CNN
+        from loop_modules import ResNet_mtat as ResNet
+        
+        self.conv_2d = ResNet(input_channels=n_harmonic, conv_channels=conv_channels)
+
+    def forward(self, x):
+        # harmonic stft
+        hstft = self.hstft(x)
+        x = self.hstft_bn(hstft)  #x = self.hstft_bn(self.hstft(x))
+
+        # 2D CNN
+        logits = self.conv_2d(x)
+
+        return logits
